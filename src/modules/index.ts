4f44d86d1de1de0c1fc64f4dd6e4610abcde237e
Damir Perkosan <damir.perkosan@diwanee.com>
2020-01-24 16:59:23 +0100

    sylius

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,11 +3,13 @@ import { DefaultCatalogModule } from './default-catalog'
 import { DefaultImgModule } from './default-img'
 import { SampleApiModule } from './sample-api'
 import { TemplateModule } from './template-module'
+import { SyliusModule } from './sylius'
 
 export default [
   DefaultVuestorefrontApiModule,
   DefaultCatalogModule,
   DefaultImgModule,
   SampleApiModule,
-  TemplateModule
+  TemplateModule,
+  SyliusModule
 ]
